5a76cd8e9c62a009cd39eff8c5db978bdb43662a
admin <chodh@ipartners.co.kr>
2022-05-31 11:11:45 +0900

    json-server 추가

--- src/main.js
+++ src/main.js
@@ -1,3 +1,9 @@
 import './scss/main.scss'
 
-console.log('aaa');
+const test = () => {
+    const _img = document.createElement('img');
+    _img.src = './img/temp_1_2.png';
+    document.body.append(_img);
+}
+
+test();
