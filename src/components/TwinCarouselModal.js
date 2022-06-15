cb115cf32cded9a53177c98d0bbfa4a67eff8258
정수진 <sssjsjj@ipartners.co.kr>
2022-06-15 13:10:36 +0900

    feat: 최적화 - 클릭 시 모달 삽입

--- src/components/TwinCarouselModal.js
+++ src/components/TwinCarouselModal.js
@@ -6,7 +6,9 @@ import '@/scss/components/TwinCarouselModal.scss'
 
 export default class TwinCarouselModal extends Component {
   setup() {
-
+    this.$state = {
+      isPopupRendered: false
+    }
   }
   template() {
     const TwinCarouselModal = newElement('div', 'carousel-twin-modal')
@@ -16,18 +18,57 @@ export default class TwinCarouselModal extends Component {
     const {dataList} = this.$props
     const targetCarouselPosts1 = newElement('div', 'carousel-main')
     this.$target.appendChild(targetCarouselPosts1)
+
     const carouselMain = await new Carousel(targetCarouselPosts1, {
       dataList,
       type: 'center',
       slideNumPerView: 3
     })
-    
 
+    const slideButtons = carouselMain.$target.querySelectorAll('.slide > button')
+    console.log('this.$state.isPopupRendered', this.$state.isPopupRendered)
+    
+      slideButtons.forEach(button => {
+        this.setEvent(button, 'click', async () => {
+          if(!this.$state.isPopupRendered) {
+            this.$state.isPopupRendered = true
+            const targetPostModal = newElement('div', 'modal-carousel')
+            this.$target.appendChild(targetPostModal)
+            const modal = await new Modal(targetPostModal, {
+              openButtons: slideButtons
+            })
+            
+            const setupModalCarouselData = (dataHasModal) => {
+              return dataHasModal.map(({ modal }) => ({
+                tit: modal.tit,
+                img: modal.pcLink,
+                alt: modal.alt,
+                description: modal.script,
+              }))
+            }
+            const carouselSub = new Carousel(modal.$contentsArea, {
+              dataList: setupModalCarouselData(dataList),
+            })
+
+            modal.toggleActive()
+
+            carouselMain.$props.twin = carouselSub
+            carouselSub.$props.twin = carouselMain
+            this.setState({
+              carouselMain,
+              carouselSub,
+              modal
+            })
+          }
+        })
+      })
+
+  }
+  async setPopup(openButtons) {
     const targetPostModal = newElement('div', 'modal-carousel')
     this.$target.appendChild(targetPostModal)
-    console.log('ELELEL', carouselMain.$target.querySelectorAll('.slide > button'))
     const modal = await new Modal(targetPostModal, {
-      openButtons: carouselMain.$target.querySelectorAll('.slide > button')
+      openButtons
     })
     
     const setupModalCarouselData = (dataHasModal) => {
